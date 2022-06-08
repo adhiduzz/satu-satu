d7cd4be17944afab864216ecd3ae1405573782a0
adhiduzz <104109164+adhiduzz@users.noreply.github.com>
2022-06-08 16:21:45 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -7,14 +7,14 @@ module.exports = {
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your Vercel's environment variable 'NEXT_PUBLIC_USER_PRINCIPLE_NAME' if you worry about
   // your email being exposed in public.
-  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'spencer@spwoo.onmicrosoft.com',
+  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'adhiduzz01@adhiduzz.com',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "Issenn Knight Drive",
+  title: "satu-satu",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
@@ -40,10 +40,9 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  // protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
-  protectedRoutes: ['/System Volume Information', '/h', '/tmp/youjiu', '/tmp/570', '/game'],
+   protectedRoutes: ['/plm/trial1', '/plm/trial2'],
 
-  hiddenRoutes: ['/System Volume Information', '/h'],
+  hiddenRoutes: ['/plm/trial1'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   // email: 'mailto:spencer.wushangbo@gmail.com',
